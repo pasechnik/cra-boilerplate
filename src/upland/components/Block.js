b28a547f626be9c6c1ec1b81e1bebf5ca9fa9537
Артур Репин <arthur@MacBook-Air-Artur.local>
2018-07-08 18:21:36 +0300

    upland

--- src/upland/components/Block.js
+++ src/upland/components/Block.js
@@ -10,23 +10,22 @@ class Block extends Component {
       owned: this.props.block.owned,
     }
   }
-  handleOwn = () => {
-    this.setState({ owned: !this.state.owned })
-  }
 
   render() {
-    const block = this.props.block
+    const {id, owned, owner, price, width, currency, square} = this.props.block
+    const ownedBlock = this.props.ownedList.indexOf(id) != -1
     return (
-      <div className={`w-${block.width} block-item-wrapper`}>
+      <div className={`w-${width} block-item-wrapper`}>
         <div
-          onClick={this.handleOwn}
+          onClick={() => this.props.toggleModal(this.props.block)}
           style={{ height: 'auto' }}
-          className={` block-item ${this.state.owned ? 'owned-block' : ''}`}
+          className={` block-item ${ownedBlock ? 'owned-block' : ''}`}
         >
-          <span><b>Owner</b>: {block.owner}</span>
-          <span><b>Price</b>: {block.currency}{block.price}</span>
-          <span><b>S</b>: {block.square}<sup>2</sup></span>
+          <span><b>Owner</b>: {owner}</span>
+          <span><b>Price</b>: {currency}{price}</span>
+          <span><b>S</b>: {square}<sup>2</sup></span>
         </div>
+
       </div>
     )
   }
--- src/upland/index.js
+++ src/upland/index.js
@@ -4,8 +4,14 @@ import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import ReactGA from 'react-ga'
 import { Route, Switch, Link } from 'react-router-dom'
-import { Container, Row, Col, Button, Label, Input } from 'reactstrap'
+import {
+  Container, Row, Col,
+  Button,
+  Label, Input,
+  Modal, ModalHeader, ModalBody, ModalFooter
+} from 'reactstrap'
 import Block from './components/Block'
+import * as Material from 'react-icons/lib/md'
 import { itemChange } from './actions/itemChange'
 import { obj } from 'the-utils'
 
@@ -16,7 +22,46 @@ import './style.min.css'
 class Upland extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      modal: false,
+      blockInfo: {
+        id: '',
+        width: '',
+        length: '',
+        square: '',
+        owner: '',
+        owned: '',
+        price: '',
+        currency: '$',
+      },
+      ownedList: [],
+    }
   }
+
+  toggleModal = blockInfo => {
+   this.setState({
+     modal: !this.state.modal,
+     blockInfo,
+   })
+ }
+
+ buyBlock = () => {
+   const blockId = this.state.blockInfo.id
+   let {ownedList} = this.state
+   ownedList.indexOf(blockId) != -1 ? ownedList : ownedList.push(blockId)
+   this.setState({
+     modal: !this.state.modal,
+     ownedList
+   })
+ }
+ sellBlock = () => {
+   const ownedList = this.state.ownedList.filter(id => id != this.state.blockInfo.id)
+   this.setState({
+     modal: !this.state.modal,
+     ownedList
+   })
+ }
 
   render() {
     const blockQuantity = []
@@ -29,11 +74,12 @@ class Upland extends Component {
         owner: 'undefined',
         owned: false,
         price: 50,
+        priceDiff: Math.floor(Math.random() * 40) + 1,
         currency: '$',
+        privateKey: Math.floor(Math.random() * 10000000000000000) + 1  ,
       }
       blockQuantity.push(block)
     }
-    console.log(blockQuantity)
     return (
       <div id='upland'>
         <div className='container-fluid'>
@@ -46,12 +92,42 @@ class Upland extends Component {
             <Col md={{ size: 12 }}>
               <div className='upland-container'>
                 { blockQuantity.map(block => (
-                  <Block key={block.id} block={block} />
+                  <Block
+                    key={block.id}
+                    block={block}
+                    toggleModal={this.toggleModal}
+                    ownedList={this.state.ownedList}
+                  />
                 ))}
               </div>
             </Col>
           </Row>
         </div>
+        <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+          <ModalHeader toggle={this.toggleModal}>Property Title #{this.state.blockInfo.id}</ModalHeader>
+          <ModalBody style={{textAlign: 'center'}}>
+            <div>Title number : #{this.state.blockInfo.id}</div>
+            <div>Title address : W 21 st 5th Ave</div>
+            <div>Flatiron District, New York City, NY 10036</div>
+            <div style={{margin: '10px 0'}}><img src={require('./includes/img/titleMap.png')} /></div>
+            <Row>
+              <Col md={{size: 4}}>
+                <Material.MdHome color="blue" /> <span>none</span>
+              </Col>
+              <Col md={{size: 4}}>
+                <Material.MdTexture color="blue" /> <span>{this.state.blockInfo.square}<sup>2</sup></span>
+              </Col>
+              <Col md={{size: 4}}>
+                <Material.MdShowChart color="blue" /> <span>{this.state.blockInfo.priceDiff}%</span>
+              </Col>
+            </Row>
+            <div style={{paddingTop: 15}}><span style={{color: 'blue'}}>{this.state.blockInfo.privateKey}</span> <b>PRIVATE KEY</b></div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.buyBlock}>Buy Title</Button>
+            <Button color="secondary" onClick={this.sellBlock}>Sell Title</Button>
+          </ModalFooter>
+        </Modal>
       </div>
     )
   }
