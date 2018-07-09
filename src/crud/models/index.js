f38aa1f27fef79d7e565dd916774f155111cfd10
arturinnovecs <artur.riepin@innovecs.com>
2018-07-09 18:13:52 +0300

    lint

---
+++ src/crud/models/index.js
@@ -0,0 +1,10 @@
+import _application from './application'
+import _notification from './notification'
+
+export const application = _application
+export const notification = _notification
+
+export default {
+  application,
+  notification,
+}
---
+++ src/crud/models/notification.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+
+const notification = {
+  propTypes: {
+    id: PropTypes.string,
+    message: PropTypes.string,
+    type: PropTypes.string,
+  },
+}
+
+export default notification
