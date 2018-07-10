eb5272764313a07f8a09ecac179939e458d03def
arturinnovecs <artur.riepin@innovecs.com>
2018-07-10 12:54:37 +0300

    deposit

--- src/common/components/Header.js
+++ src/common/components/Header.js
@@ -29,6 +29,8 @@ class Header extends Component {
   }
 
   render() {
+    const depositPath = window.location.pathname === '/deposit'
+    console.log(depositPath)
     return (
       <header>
         <Container>
@@ -36,8 +38,8 @@ class Header extends Component {
             <IndexLinkContainer to='/'>
               <NavbarBrand>reactstrap</NavbarBrand>
             </IndexLinkContainer>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
+            <NavbarToggler onClick={this.toggle} style={depositPath ? {visibility: 'hidden'} : null} />
+            <Collapse isOpen={this.state.isOpen} style={depositPath ? {visibility: 'hidden'} : null} navbar>
               <Nav className='ml-auto' navbar>
                 <NavItem>
                   <IndexLinkContainer to='/'>
