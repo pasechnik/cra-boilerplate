4ae69704f44323c97482ad60733c0a1c3aa22d7a
Vladimir Pasechnik <vladimir.pasechnik@gmail.com>
2018-07-10 13:20:08 +0300

    fix

--- src/deposit_mobile/epics/fetchData.js
+++ src/deposit_mobile/epics/fetchData.js
@@ -20,7 +20,7 @@ import {
   // REQUEST_QUOTES_FAILED,
 } from '../actions/consts'
 
-import { makeDataRequestSucceed } from '../actions/makeDepositRequest'
+import { makeDepositRequestSucceed } from '../actions/makeDepositRequest'
 
 const url = 'http://localhost:4004/mz_cashier_get_general_settings_front'
 // epic
@@ -28,7 +28,7 @@ const fetchDataEpic = action$ => action$
   .ofType(FETCH_DATA_REQUEST)
   .mergeMap(action =>
     Observable.ajax.get(url, { application: action.payload }, { 'Content-Type': 'application/json; charset=utf-8' })
-      .map(response => makeDataRequestSucceed(response.response))
+      .map(response => makeDepositRequestSucceed(response.response))
       .catch((error) => {
         console.log(error)
         return Observable.of({
