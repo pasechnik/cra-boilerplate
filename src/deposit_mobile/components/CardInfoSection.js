b7bf0c88186a530a45ac402050198bcc2d4424ca
Vladimir Pasechnik <vladimir.pasechnik@gmail.com>
2018-07-11 11:35:15 +0300

    fix eslint

--- src/deposit_mobile/components/CardInfoSection.js
+++ src/deposit_mobile/components/CardInfoSection.js
@@ -15,20 +15,22 @@ for (let i = 1; i <= 31; i += 1) {
   }
 }
 
-const CardInfoSection = (props) => {
-  const creditCard = obj.deep(props, ['accountInfo', 'credit_card_number'], '')
-  const cvv = obj.deep(props, ['accountInfo', 'exp_date_cvv'], '')
+const CardInfoSection = ({
+  accountInfo, firstLoad, onTextChange, onSelectChange,
+}) => {
+  const creditCard = obj.get(accountInfo, 'credit_card_number', '')
+  const cvv = obj.get(accountInfo, 'exp_date_cvv', '')
   return (
     <div className='card-number-wrapper'>
       <Row>
         <Col xs={{ size: 12 }} md={{ size: 12 }}>
-          <FormGroup validationState={(creditCard.length !== 16 && !props.firstLoad.number) ? 'error' : null}>
+          <FormGroup validationState={(creditCard.length !== 16 && !firstLoad.number) ? 'error' : null}>
             <ControlLabel>
-Credit Card Number
+              Credit Card Number
             </ControlLabel>
             <FormControl
               name='credit_card_number'
-              onChange={e => props.onTextChange(e, 'credit_card_number')}
+              onChange={e => onTextChange(e, 'credit_card_number')}
               type='number'
               value={creditCard}
               pattern='[0-9]*'
@@ -40,13 +42,13 @@ Credit Card Number
       <Row>
         <Col xs={{ size: 9 }}>
           <Label for='exp_date'>
-Expiration Date
+            Expiration Date
           </Label>
           <Row>
             <Col xs={{ size: 5 }} className='month_div selectDiv'>
               <Input
                 name='exp_date_month'
-                onChange={e => props.onSelectChange(e, 'exp_date_month')}
+                onChange={e => onSelectChange(e, 'exp_date_month')}
                 type='select'
                 placeholder='MM'
               >
@@ -60,7 +62,7 @@ Expiration Date
             <Col xs={{ size: 6 }} className='selectDiv'>
               <Input
                 name='exp_date_year'
-                onChange={e => props.onSelectChange(e, 'exp_date_year')}
+                onChange={e => onSelectChange(e, 'exp_date_year')}
                 type='select'
                 placeholder='YY'
               >
@@ -74,13 +76,13 @@ Expiration Date
           </Row>
         </Col>
         <Col xs={{ size: 3 }} style={{ paddingLeft: 0 }}>
-          <FormGroup validationState={(cvv.length !== 3 && !props.firstLoad.cvv) ? 'error' : null}>
+          <FormGroup validationState={(cvv.length !== 3 && !firstLoad.cvv) ? 'error' : null}>
             <ControlLabel>
-CVV
+              CVV
             </ControlLabel>
             <FormControl
               name='exp_date_cvv'
-              onChange={e => props.onTextChange(e, 'exp_date_cvv')}
+              onChange={e => onTextChange(e, 'exp_date_cvv')}
               type='number'
               pattern='[0-9]*'
               value={cvv}
@@ -94,6 +96,10 @@ CVV
 }
 
 CardInfoSection.propTypes = {
+  accountInfo: PropTypes.shape({
+    credit_card_number: PropTypes.string,
+    exp_date_cvv: PropTypes.string,
+  }).isRequired,
   firstLoad: PropTypes.shape({
     number: PropTypes.bool,
     cvv: PropTypes.bool,
